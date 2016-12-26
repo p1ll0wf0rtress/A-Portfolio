84e4780f40ee0a7b1aa8a890f141e944fc9d5cdd
p1ll0wf0rtress <devin.santamaria@gmail.com>
2016-12-25 17:45:18 -0800

    Begin modal work

--- public/scripts/ashley.js
+++ public/scripts/ashley.js
@@ -1,12 +1,8 @@
+//fullpage
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        anchors:['top', 'about', 'work', 'contact']
+        anchors: ['top', 'about', 'work', 'contact']
     });
-});
-
-$('.aboutText').flowtype({
-    minFont: 16,
-    maxFont: 40,
 });
 
 $(".about").click(function() {
@@ -22,6 +18,13 @@ $(".contact").click(function() {
     $.fn.fullpage.moveTo(4);
 });
 
+//flowtype
+$('.aboutText').flowtype({
+    minFont: 16,
+    maxFont: 40,
+});
+
+//check for webp support
 Modernizr.on('webp', function(result) {
     if (result == true) {
         console.log("Webp supported");
@@ -36,3 +39,31 @@ Modernizr.on('webp', function(result) {
         alert("everything's broken!");
     }
 });
+
+//// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("case1");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    console.log("clicked");
+    modal.style.display = "block";
+    $.fn.fullpage.controlArrows == false;
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
