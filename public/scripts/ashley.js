eb7172aff6686cb1ae69a794e858c520c543c7dc
p1ll0wf0rtress <devin.santamaria@gmail.com>
2016-12-27 23:25:48 -0800

    Attempt dynamic modals

--- public/scripts/ashley.js
+++ public/scripts/ashley.js
@@ -41,29 +41,39 @@ Modernizr.on('webp', function(result) {
 });
 
 //// Get the modal
-var modal = document.getElementById('myModal');
-
+var modal1 = document.getElementById('modal1');
+var modal2 = document.getElementById('modal2');
+var modal3 = document.getElementById('modal3');
+var modal4 = document.getElementById('modal4');
 // Get the button that opens the modal
-var btn = document.getElementById("case1");
-
+var btn1 = document.getElementById("case1");
+var btn2 = document.getElementById("case2");
+var btn3 = document.getElementById("case3");
+var btn4 = document.getElementById("case4");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn1.onclick = function() {
     console.log("clicked");
-    modal.style.display = "block";
-    $.fn.fullpage.controlArrows == false;
+    modal1.style.display = "block";
+    $.fn.fullpage.controlArrows = false;
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modal1.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+    else if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+    else if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+    else if (event.target == modal4) {
+        moda4.style.display = "none";
     }
 }
